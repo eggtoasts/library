2fe66396a3e891f769b96e792bb3f6aff8d13b72
eggtoasts <129806552+eggtoasts@users.noreply.github.com>
2025-09-21 15:15:33 -0400

    Added header, sidebar, books container, and form. (Basic layout)

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 const myLibrary = [];
+
+const deleteBookButton = document.querySelector(".deleteBook");
+const addBookButton = document.querySelector(".addBook");
+
+//Constructor for Book object
+function Book(name, author, pages, isFinished) {
+  this.name = name;
+  this.author = author;
+  this.pages = pages;
+  this.isFinished = isFinished;
+
+  //Gives our book a unique id
+  this.id = crypto.randomUUID();
+}
+
+// Adds book into our array.
+function addBookToLibrary(name, author, pages, isFinished) {
+  let book = new Book(name, author, pages, isFinished);
+
+  myLibrary.push(book);
+
+  //creates Book object.
+  const bookItem = document.createElement("books-item");
+  const bookCover = bookItem.createElement("book-cover");
+  const name = bookCover.createElement("name");
+}
+
+//create a book as an example.
